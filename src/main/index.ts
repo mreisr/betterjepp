23520df3123f1fee877b06e08d841b0d17488ea5
StarNumber12046 <starnumber.official12046@gmail.com>
2026-03-08 14:48:21 +0100

    Added debug mode

--- src/main/index.ts
+++ src/main/index.ts
@@ -34,6 +34,9 @@ function createWindow(): void {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+  if (process.argv.includes('--debug')) {
+    mainWindow.webContents.openDevTools()
+  }
 }
 
 // This method will be called when Electron has finished
@@ -42,7 +45,6 @@ function createWindow(): void {
 app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
-
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
